37147acc978521f714a3e57ac11df4a434d5dae3
Geraint White <geraintwhite@gmail.com>
2022-05-23 11:56:19 +0100

    Allow options to be passed to takeScreenShot

--- src/index.js
+++ src/index.js
@@ -21,11 +21,11 @@ const useScreenshot = ({ type, quality } = {}) => {
    * convert html node to image
    * @param {HTMLElement} node
    */
-  const takeScreenShot = (node) => {
+  const takeScreenShot = (node, options = {}) => {
     if (!node) {
       throw new Error('You should provide correct html node.')
     }
-    return html2canvas(node)
+    return html2canvas(node, options)
       .then((canvas) => {
         const croppedCanvas = document.createElement('canvas')
         const croppedCanvasContext = croppedCanvas.getContext('2d')
